00233c8fccd149e3c7483168afccb927df7d129c
adaxiik <a.jantula@seznam.cz>
2022-06-21 15:12:59 +0200

    renamed build-md to docs

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 set -xe
 
-if [ -d "build-md/assets" ]; then
-    rm -rf build-md/assets
+if [ -d "docs/assets" ]; then
+    rm -rf docs/assets
 else  
-    mkdir -p build-md/assets
+    mkdir -p docs/assets
 fi
 
-cp -r assets build-md
-pandoc APPS.MD -f markdown -t html -s -o build-md/APPS.html
-pandoc APPS.MD -f markdown -t pdf -s -o build-md/APPS.pdf
-pandoc APPS.MD -f markdown -t docx -s -o build-md/APPS.docx
+cp -r assets docs
+pandoc APPS.MD -f markdown -t html -s -o docs/APPS.html
+pandoc APPS.MD -f markdown -t pdf -s -o docs/APPS.pdf
+pandoc APPS.MD -f markdown -t docx -s -o docs/APPS.docx
 
-# if [ -e build-md/APPS.html ]
+# if [ -e docs/APPS.html ]
 # then
-#     $1 --headless --disable-gpu --hide-scrollbars --window-size=800,10000  --screenshot=build-md/APPS.png build-md/APPS.html
+#     $1 --headless --disable-gpu --hide-scrollbars --window-size=800,10000  --screenshot=docs/APPS.png docs/APPS.html
 # fi
 
